c0569058fba4cf65131d0a9f3986eefedb67db7a
sushi-chaaaan <sushi_code@outlook.jp>
2022-07-20 10:46:32 +0900

    add method 'inputted_values' to 'Modal'

--- discord/ext/ui/modal.py
+++ discord/ext/ui/modal.py
@@ -14,13 +14,19 @@ class Modal(ui.Modal):
             self.add_item(component)
 
         self._hook = None
+        self._components = components
 
     def hook(self, func: Callable[[discord.Interaction], Any]) -> Modal:
         self._hook = func
         return self
 
     async def on_submit(self, interaction: discord.Interaction) -> None:
+        inputted_values = []
+        for raw_component in interaction.data.get("components", []): # type: ignore
+            for component in self._components:
+                if raw_component.get("custom_id") == component.custom_id:
+                    inputted_values.append({f"{component.label}" : raw_component.get("value")})
         if self._hook is not None:
-            await _call_any(self._hook, interaction)
+            await _call_any(self._hook, interaction, inputted_values)
         if not interaction.response.is_done():
             await interaction.response.defer()
