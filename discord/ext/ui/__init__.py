c5a69b6c044daa0535000778c73e791b293beabd
sushi-chaaaan <sushi_code@outlook.jp>
2022-12-29 21:40:38 +0900

    importがうまく解決されない問題の修正

--- discord/ext/ui/__init__.py
+++ discord/ext/ui/__init__.py
@@ -1,20 +1,27 @@
 # flake8: noqa
-from .view import View
-from .tracker import ViewTracker
-from .provider import MessageProvider, InteractionProvider
-from .button import LinkButton, Button
-from .message import Message
-from .observable_object import ObservableObject
-from .state import state
-from .published import published
-from .select import SelectOption, Select
-from .page import PaginationView, PaginationButtons, PageView
-from .alert import Alert, ActionButton
-from .modal import Modal
+from .view import View as View
+from .tracker import ViewTracker as ViewTracker
+from .provider import (
+    MessageProvider as MessageProvider,
+    InteractionProvider as InteractionProvider,
+)
+from .button import LinkButton as LinkButton, Button as Button
+from .message import Message as Message
+from .observable_object import ObservableObject as ObservableObject
+from .state import state as state
+from .published import published as published
+from .select import SelectOption as SelectOption, Select as Select
+from .page import (
+    PaginationView as PaginationView,
+    PaginationButtons as PaginationButtons,
+    PageView as PageView,
+)
+from .alert import Alert as Alert, ActionButton as ActionButton
+from .modal import Modal as Modal
 
 
-__title__ = 'discord.ext.ui'
-__author__ = 'sizumita'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2020-present sizumita'
+__title__ = "discord.ext.ui"
+__author__ = "sizumita"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020-present sizumita"
 __version__ = "3.1.9"
